6803c53ff7db92cfeccaaf2537d7d94cf40ee71d
bpwolf <bpisabaj@gmail.com>
2026-08-09 14:31:06 -0500

    refactored Coffee.Money package structure to coffeemoney

--- backend/src/main/java/coffeemoney/CoffeeMoneyApplication.java
+++ backend/src/main/java/coffeemoney/CoffeeMoneyApplication.java
@@ -1,4 +1,4 @@
-package Coffee.Money;
+package coffeemoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/coffeemoney/CoffeeMoneyApplicationTests.java
+++ backend/src/test/java/coffeemoney/CoffeeMoneyApplicationTests.java
@@ -1,4 +1,4 @@
-package Coffee.Money;
+package coffeemoney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
